b2d0f1b273db0b12f79b7c7625f4658dc12fbd93
taro5430 <abtaabta@gmail.com>
2021-09-10 22:15:54 +0900

    図書館一覧ページを表示させるためのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   devise_for :users
   resources :libraries
+  get '/libraries_display' ,to: 'libraries#display'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
