56560148ca1a3445814e09538c815f90d94f2b0f
taro5430 <abtaabta@gmail.com>
2021-09-13 21:53:55 +0900

    validationの追加及び住所から経度緯度を算出するよう設定

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,6 +1,9 @@
 class Library < ApplicationRecord
-  validates :name, :address, :study_space, :electrical_outlet, presence:true
+  validates :name, :address, :access, :study_space, :electrical_outlet, presence:true
   
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   belongs_to :user
 
   has_one_attached :avatar
