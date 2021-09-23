40a8b24606fbb66a2aa0188bad731e613d2ee8a2
taro5430 <abtaabta@gmail.com>
2021-09-23 13:47:50 +0900

    名前を必須にするようvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :libraries
+         
+  validates :name, presence:true
 
+  has_many :libraries
   has_one_attached :avatar
 end
