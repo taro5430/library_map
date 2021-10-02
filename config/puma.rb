2b86c8bbab9809fa6cb21c0ccac4c6bc941e452b
taro5430 <83792679+taro5430@users.noreply.github.com>
2021-10-02 13:11:11 +0900

    Revert "デプロイ設定を修正"

--- config/puma.rb
+++ config/puma.rb
@@ -15,9 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-#port ENV.fetch("PORT") { 3000 }
-app_dir = File.expand_path("../..", __FILE__)
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
