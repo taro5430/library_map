ce18374a2b6535779a890b4bbd4b41e0ec71c3d8
taro5430 <abtaabta@gmail.com>
2021-11-27 14:15:34 +0900

    管理者機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/introduction'
   get 'pages/profile'
-  devise_for :users
+  get 'pages/admin'
   get '/libraries/search', to: 'libraries#search'
   resources :libraries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
