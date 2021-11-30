0753ee9fcff24effdac2a03b12e139e788ab2bc3
taro5430 <abtaabta@gmail.com>
2021-11-30 11:14:04 +0900

    guestメソッドのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
     end
   end
+
+  describe 'self.guest' do
+    let!(:existing_user) { create(:user) }
+    let!(:guest_user) { User.guest }
+
+    it 'create guest user when there is no guest user' do
+      expect(guest_user).to be_valid
+      expect(guest_user.email).to eq 'guest@example.com'
+      expect(guest_user.name).to eq 'guest_user'
+      expect(User.count).to eq 2
+    end
+
+    it "doesn't create guest_user when a guest user already exists" do
+      guest_user2 = User.guest
+      expect(User.count).to eq 2
+      expect(guest_user).to eq guest_user2
+    end
+  end
 end
